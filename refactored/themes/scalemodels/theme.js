b2ffd7bda29c1674f165ccc4a643323abc48bfb4
Alexander Vakhlov <vakhlov@gmail.com>
2020-12-30 14:30:17 +0500

    Переверстал главное меню; вынес часть стилей и скриптов во внешние файлы

--- refactored/themes/scalemodels/theme.js
+++ refactored/themes/scalemodels/theme.js
@@ -23,38 +23,38 @@ window.onload = function(e){
 
 $(document).ready(function() {
 
-	try {
-        // SmartMenus init
-        if($('#main-menu') && 'smartmenus' in $('#main-menu')) {
-            $(function() {
-                $('#main-menu').smartmenus({
-                    subMenusSubOffsetX: 1,
-                    subMenusSubOffsetY: -8
-                });
-            });
-            // SmartMenus mobile menu toggle button
-            $(function() {
-                var $mainMenuState = $('#main-menu-state');
-                if ($mainMenuState.length) {
-                // animate mobile menu
-                    $mainMenuState.change(function(e) {
-                        var $menu = $('#main-menu');
-                        if (this.checked) {
-                            $menu.hide().slideDown(250, function() { $menu.css('display', ''); });
-                        } else {
-                            $menu.show().slideUp(250, function() { $menu.css('display', ''); });
-                        }
-                    });
-                    // hide mobile menu beforeunload
-                    $(window).bind('beforeunload unload', function() {
-                        if ($mainMenuState[0].checked) {
-                            $mainMenuState[0].click();
-                        }
-                    });
-                }
-            });
-        }
-    } catch ($e) {}
+	// try {
+    //     // SmartMenus init
+    //     if($('#main-menu') && 'smartmenus' in $('#main-menu')) {
+    //         $(function() {
+    //             $('#main-menu').smartmenus({
+    //                 subMenusSubOffsetX: 1,
+    //                 subMenusSubOffsetY: -8
+    //             });
+    //         });
+    //         // SmartMenus mobile menu toggle button
+    //         $(function() {
+    //             var $mainMenuState = $('#main-menu-state');
+    //             if ($mainMenuState.length) {
+    //             // animate mobile menu
+    //                 $mainMenuState.change(function(e) {
+    //                     var $menu = $('#main-menu');
+    //                     if (this.checked) {
+    //                         $menu.hide().slideDown(250, function() { $menu.css('display', ''); });
+    //                     } else {
+    //                         $menu.show().slideUp(250, function() { $menu.css('display', ''); });
+    //                     }
+    //                 });
+    //                 // hide mobile menu beforeunload
+    //                 $(window).bind('beforeunload unload', function() {
+    //                     if ($mainMenuState[0].checked) {
+    //                         $mainMenuState[0].click();
+    //                     }
+    //                 });
+    //             }
+    //         });
+    //     }
+    // } catch ($e) {}
 
     try {
         $('.imgLiquid').imgLiquid(); 
